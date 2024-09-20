5bfc1670b1c80980be75d12414cecdd696963f1f
Kajetan Szczurowski <kajetan.szczurowski@gmail.com>
2024-09-20 14:20:12 +0200

    Inserting is fine. Now working on removing Nodes from Linked List

--- Sandbox_Cpp/LinkedList.cpp
+++ Sandbox_Cpp/LinkedList.cpp
@@ -1,19 +1,22 @@
 #include <memory>
 #include <iostream>
+#include <string>
 
-struct Node {
-	int value;
-	std::unique_ptr<Node> next;
-	Node(int initialValue = 0) : value{ initialValue }, next{ nullptr } {}
-};
+template <typename T> class SinglyLinkedList {
 
-class SinglyLinkedList {
+	struct Node {
+		T value;
+		std::unique_ptr<Node> next;
+		Node(T initialValue = 0) : value{ initialValue }, next{ nullptr } {}
+	};
+
 public:
 
-	SinglyLinkedList() : head{ nullptr } {};
+	SinglyLinkedList() : head{ nullptr }, length{0} {};
 
 
-	void insertAtEnd(int value) {
+	void insertAtEnd(T value) {
+		length++;
 		std::unique_ptr<Node> newNode{ std::make_unique<Node>(value) };
 		if (!head) {
 			head = std::move(newNode);
@@ -24,7 +27,8 @@ public:
 		lastNode->next = std::move(newNode);
 	}
 
-	void insertAtBeginning(int value) {
+	void insertAtBeginning(T value) {
+		length++;
 		std::unique_ptr<Node> newNode{ std::make_unique<Node>(value) };
 		if (!head) {
 			head = std::move(newNode);
@@ -32,6 +36,35 @@ public:
 		}
 		newNode->next = std::move(head);
 		head = std::move(newNode);
+	}
+
+	void insertAtPosition(T value, short position) {
+		if (position < 0) return;
+		if (position == 0) {
+			insertAtBeginning(value);
+			return;
+		}
+		if (position >= length) {
+			insertAtEnd(value);
+			return;
+		}
+		length++;
+		std::unique_ptr<Node> newNode{ std::make_unique<Node>(value) };
+		Node* previousNode = iterate(FIND, position -1);
+		std::unique_ptr<Node> nextNode = std::move(previousNode->next);
+		newNode.get()->next = std::move(nextNode);
+		previousNode->next = std::move(newNode);
+	}
+
+	void removeFromEnd() {
+		if (head == nullptr) return;
+		if (length == 1) {
+			head = nullptr;
+			return;
+		}
+		Node* newLastNode = iterate(FIND, length - 2);
+		std::cout << newLastNode->value;
+		newLastNode->next = nullptr;
 	}
 
 	Node* getHead() {
@@ -45,17 +78,22 @@ public:
 
 private:
 	std::unique_ptr<Node> head;
+	unsigned short length;
 
 	const enum ITERATION_ORDERS { PRINT, FIND, FIND_LAST };
 
-	Node* iterate(enum ITERATION_ORDERS order = FIND_LAST, unsigned index = -1) {
+	Node* iterate(enum ITERATION_ORDERS order = FIND_LAST, short position = -1) {
 		if (!head) return nullptr;
+		short currentNodeIndex = 0;
 		std::unique_ptr<Node> temporary;
 		Node* current = head.get();
+		if (order == FIND && position <= 0) return current;
 		while (current != nullptr) {
+			if (order == FIND && position == currentNodeIndex) return current;
 			if (order == PRINT) std::cout << current->value << " -> "; //TODO: remove arrow from last element
 			if (!current->next) return current;
 			current = current->next.get();
+			currentNodeIndex++;
 		}
 		return nullptr;
 	}
@@ -73,13 +111,51 @@ private:
 
 void workWithLinkedList() {
 
-	SinglyLinkedList myList;
-	myList.insertAtEnd(28);
-	myList.insertAtEnd(13);
-	myList.insertAtBeginning(41);
-	myList.insertAtEnd(7);
-	//std::cout << myList.getHead()->value;
-	myList.printList();
+	//SinglyLinkedList<int> myList;
+	//myList.insertAtEnd(28);
+	//myList.insertAtEnd(13);
+	//myList.insertAtBeginning(41);
+	//myList.insertAtEnd(7);
+	//myList.printList();
+
+	//std::cout << std::endl;
+
+	//SinglyLinkedList<std::string> textList;
+	//textList.insertAtEnd("Hi");
+	//textList.insertAtEnd("I am a text Linked List");
+	//textList.insertAtEnd("My name is Andrew");
+	//textList.printList();
+
+	SinglyLinkedList<int> intList;
+	intList.insertAtEnd(5);
+	intList.printList();
+	std::cout << std::endl;
+
+	intList.removeFromEnd();
+	std::cout << std::endl;
+	intList.insertAtEnd(6);
+	intList.printList();
+	std::cout << std::endl;
+	intList.removeFromEnd();
+	std::cout << std::endl;
+
+	intList.insertAtEnd(8);
+	intList.removeFromEnd();
+	std::cout << std::endl;
+
+	intList.printList();
+	std::cout << std::endl;
+	intList.insertAtPosition(2, 0);
+	intList.insertAtPosition(16, 1);
+	intList.insertAtPosition(100, 800);
+	intList.insertAtPosition(64, 3);
+	intList.printList();
+	std::cout << std::endl;
+
+	intList.removeFromEnd();
+	
+
+
 
 	std::cin.get();
 }
